a89b10a1277243adbfad646250983bedc00a3b56
itsinfi <sascha.angermann2002@gmail.com>
2024-05-21 19:49:34 +0200

    fix for only creating one session at a time

--- src/utils/SessionCreator.jsx
+++ src/utils/SessionCreator.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import LoadingSpinner from '../components/loadingSpinner/LoadingSpinner'
 import { Navigate } from 'react-router-dom'
 import SessionService from '../services/SessionService'
@@ -17,27 +17,37 @@ function SessionCreator({ config }) {
 
     const [error, setError] = useState(null)
 
+    const inProgress = useRef(false)
+
 
     //init socket.io and check vor joining/leaving players
     useEffect(() => {
+
+        if (inProgress.current) {
+            return
+        }
 
         //send post request to create new session and return json response
         async function getNewSession() {
             return (await SessionService.createSession(config)).json().then(json => { return json})
         }
+
+        inProgress.current = true;
         
         //create sesssion and then set session storage values and set the session parameter for the new route to load
         getNewSession().then(session => {
             sessionStorage.setItem('playerNumber', session.players[0].playerNumber)
             sessionStorage.setItem('sessionCode', session.sessionCode)
             setSession(session.sessionCode)
+            inProgress.current = false;
         }).catch(err => {
             console.error(err)
             setError(new Error('Session konnte nicht erstellt werden'))
+            inProgress.current = false;
         })
 
         
-    }, [config])
+    }, [config, inProgress])
 
 
     //check if there is an error
